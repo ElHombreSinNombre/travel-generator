2750f1305e14d9c133655240731aae4dde6773c0
Asier Lara <laraasier@gmail.com>
2024-12-13 09:28:15 +0100

    Renombrar claves de API en el archivo de configuración y actualizar su uso en el código

--- app/utils/env.config.ts
+++ app/utils/env.config.ts
@@ -1,7 +1,7 @@
 const {
-  GOOGLE_API_KEY: google_api_key = '',
-  OPENAI_API_KEY: gpt_api_key = '',
-  PEXELS_API_KEY: pexels_api_key = ''
+  NEXT_PUBLIC_GOOGLE_API_KEY: google_api_key = '',
+  NEXT_PUBLIC_OPENAI_API_KEY: gpt_api_key = '',
+  NEXT_PUBLIC_PEXELS_API_KEY: pexels_api_key = ''
 } = process.env
 
 export const EnvConfig = () => ({
